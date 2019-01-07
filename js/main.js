7a9c302e5e1874f7cfb2c91085b5266a4e264867
nam <namnh620@wru.vn>
2019-01-07 21:22:45 +0700

    add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-	
-});
+
+var thoiGian = document.getElementById('thoi-gian');
+
+thoiGian.addEventListener('click', onclick);
+
+var save = [];
+var b = -1;
+
+function onclick(event) {
+	var button = event.target;
+	var btn = document.getElementsByClassName('number');
+	var i = parseInt(button.dataset.id);
+	save.push(i);
+
+	if (save.length > 1) {
+		b++;
+		var a = save[b];
+		btn[a].style.backgroundColor = '#fff';
+		btn[a].style.color = '#000';
+	}
+	btn[i].style.backgroundColor = '#000';
+	btn[i].style.color = '#fff';
+}
